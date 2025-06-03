b9b500f586f5dec6e3f1e7259716fdf78e82446d
Lama71x <webdev9849@gmail.com>
2025-06-03 13:13:33 +0000

    Lesson 1

---
+++ 01-Basic/less-1.js
@@ -0,0 +1,8 @@
+//Get user to input a number using promt ("Enter a number").Check if the number is a multiple of 5 or not.
+
+let num = prompt("Enter a number: ");
+console.log(num);
+
+let(num %== 5){
+    console.log()
+}
